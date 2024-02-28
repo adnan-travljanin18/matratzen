3cb552a45a7099bd96723c36fa0de656ea1693ad
Adnan <adnan.travljanin@hulkapps.com>
2024-02-28 21:53:22 +0100

    fixing info slider

--- assets/info-slider.js
+++ assets/info-slider.js
@@ -1,9 +1,4 @@
-var swiperScript = document.createElement('script');
-swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js';
-swiperScript.defer = true;
-
-swiperScript.onload = function () {
-  // Swiper script has loaded successfully, you can now initialize Swiper
+document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper(".swiper-container-info", {
     slidesPerView: 3,
     spaceBetween: 20,
@@ -25,14 +20,4 @@ swiperScript.onload = function () {
       }
     }
   });
-};
-
-swiperScript.onerror = function () {
-  // Handle the error, perhaps by falling back to a local copy or showing an error message
-  console.error('Failed to load Swiper script.');
-};
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   console.log('DOMContentLoaded event fired');
-  
-// });
+});
